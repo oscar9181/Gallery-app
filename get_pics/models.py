e719283c356eb9cb0a027e2fc1e6db518c35970a
oscar9181 <oscarokola17@gmail.com>
2022-05-30 14:51:55 +0300

    models

--- get_pics/models.py
+++ get_pics/models.py
@@ -1,5 +1,23 @@
+from contextlib import nullcontext
+from unicodedata import category, name
 from django.db import models
 
 # Create your models here.
 
-class Category(models.Models)
+class Category(models.Model):
+    name = models.CharField(max_length = 200,null = False,blank = False)
+     
+    def __str__(self):
+          return self.name
+      
+      
+class Pictures(models.Model):
+    
+    category = models.ForeignKey(category,on_delete=models.SET_NULL)
+    image = models.ImageField(null=False,blank=False)
+    description = models.CharField(max_length = 300, null= False,blank = False)
+    
+    def __str__(self):
+          return self.description
+      
+      
