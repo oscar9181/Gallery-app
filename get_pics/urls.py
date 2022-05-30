3b359d398a5c552a23f0ab1cf912f78cb4ed56b6
oscar9181 <oscarokola17@gmail.com>
2022-05-30 11:46:29 +0300

    add bootsrap

--- get_pics/urls.py
+++ get_pics/urls.py
@@ -1,2 +1,9 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
+
+urlpatterns = [
+    path('', views.home,name='gallery'),
+    path('picture/<str:pk>/',views.viewPicture,name='album'),
+    path('add/',views.addPicture,name='add')
+    
+    ]
--- get_pics/views.py
+++ get_pics/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+
+
+# def base(request):
+#     return render(request,'gallery/base.html')
+
+
+def home(request):
+    return render(request, 'pictures/home.html')
+
+def viewPicture(request,pk):
+    return render(request,'pictures/display.html')
+
+def addPicture(request):
+    return render(request,'pictures/add.html')
